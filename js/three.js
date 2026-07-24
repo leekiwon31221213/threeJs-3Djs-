92063201bdab37a89d6c7a29773215226b1b5f46
Lee Ki Won <cute01235@nate.com>
2026-07-24 11:15:17 +0900

    10차시 까지 공부

--- js/three.js
+++ js/three.js
@@ -23,16 +23,26 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // 박스 모양 만들기
 const getmetry = new THREE.BoxGeometry(1, 1, 1);
 
+const textureLoader = new THREE.TextureLoader(); /*  이미지를 불러오는 도구를 만듦 */
+const boxTexture = textureLoader.load("/img/bamby.png"); /* 이미지 불러오기 */
+
+// const boxTexture = textureLoader.load("https://iili.io/CO0IkAl.png");
+
 // 박스 재질 만들기 MeshStandardMaterial 빛을 받는 재질
-const meterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const meterial = new THREE.MeshStandardMaterial({ color: 0x00ff00, map: boxTexture });
+const meterial2 = new THREE.MeshStandardMaterial({ color: 0xffff00, map: boxTexture });
+const meterial3 = new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.7 /* 표면이 얼마나 거친지 정하는 값 */, metalness: 1 /* 금속 느낌을 정하는 값 */, map: boxTexture /* 이미지 맵핑 */ });
 
 // 박스 만들기
 const box = new THREE.Mesh(getmetry, meterial);
-const box2 = new THREE.Mesh(getmetry, meterial);
-const box3 = new THREE.Mesh(getmetry, meterial);
+const box2 = new THREE.Mesh(getmetry, meterial2);
+const box3 = new THREE.Mesh(getmetry, meterial3);
 
 box2.position.x = 2;
 box3.position.x = -2;
+
+box2.scale.x = 2;
+box3.scale.y = -2;
 
 // 빛 추가
 const light = new THREE.DirectionalLight(0xffffff, 1); /* DirectionalLight(빛의색,빛의밝기) 한 방향에서 비추는 빛 */
@@ -40,6 +50,33 @@ light.position.set(3, 3, 3); /* 빛의 위치 설정  x,y,z 축*/
 
 // 마우스로 화면 조작하기
 const controls = new OrbitControls(camera, renderer.domElement);
+
+// 배경색 넣기
+scene.background = new THREE.Color(0xeaf1ff);
+
+// 안개 넣기
+scene.fog = new THREE.Fog(0xeaf1ff, 1, 10); /* (색상,시작거리,끝거리) */
+/* renderer그림자 켜기 */
+renderer.shadowMap.enabled = true;
+
+/* 빛에서 그림자 켜기 */
+light.castShadow = true;
+box.castShadow = true;
+box2.castShadow = true;
+box3.castShadow = true;
+
+/* 바닥에 그림자 만들기 */
+
+const floorGeometry = new THREE.PlaneGeometry(10, 10); /* 가로10,세로10 평평한 바닥을 만든다 */
+const floorMaterial = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+});
+
+const floor = new THREE.Mesh(floorGeometry, floorMaterial); /* 재질을 만든다 */
+floor.rotation.x = -Math.PI / 2; /* 바닥을 눕힌다. */
+floor.position.y = -1;
+floor.receiveShadow = true;
+scene.add(floor);
 
 // 장면에 만든 박스 추가 3D 공간안에 넣음
 scene.add(box);
